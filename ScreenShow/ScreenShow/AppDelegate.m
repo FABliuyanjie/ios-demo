661bac96c01187f4568b389a01ab08420deaacca
jacklibonnie <1060704961@qq.com>
2014-05-20 18:30:50 +0800

    修改了推送证书出现的问题
    修改了推送证书出现的问题

--- ScreenShow/ScreenShow/AppDelegate.m
+++ ScreenShow/ScreenShow/AppDelegate.m
@@ -53,8 +53,6 @@
     //友盟
     [UMSocialData setAppKey:@"5373474456240b1cbc020bde"];
     [UMSocialWechatHandler setWXAppId:@"wxd95aad11f865fcd6" url:@"http://www.umeng.com/social"];
-//    [UMSocialQQHandler setQQWithAppId:@"801076124" appKey:@"6ddc7dc46845644ae30fe189a2a296dd" url:@"http://www.umeng.com/social"];
-    
     [UMSocialQQHandler setQQWithAppId:@"100424468" appKey:@"c7394704798a158208a74ab60104f0ba" url:@"http://www.umeng.com/social"];
 
     
@@ -89,45 +87,11 @@
     menuVC.rightVC=myNav;
     
     
-    /*
-    
-    //MARK:通知刷新用户信息
-    SendNoti(kReflushUserInfo);
-    
-    //MARK:分享
-    [ShareSDK registerApp:kShareSDKKey];
-    
-    [ShareSDK connectTencentWeiboWithAppKey:@"101003536" appSecret:@"8cb9805a1a3819f8a161f196f6a94952" redirectUri:@""];
-    //QQ空间
-    [ShareSDK connectQZoneWithAppKey:@"101003536" appSecret:@"8cb9805a1a3819f8a161f196f6a94952"];
-    //微信
-    [ShareSDK connectWeChatWithAppId:@"195f927365f5" wechatCls:[WXApi class]];
-    //添加新浪微博应用
-    [ShareSDK connectSinaWeiboWithAppKey:@"3611880415"
-                               appSecret:@"376b97e7c9e4cabe5f7d54f4af254640"
-                             redirectUri:@"http://www.fabshow.cn"];
-    
-    //添加人人网应用
-    [ShareSDK connectRenRenWithAppKey:@"fc5b8aed373c4c27a05b712acba0f8c3"
-                            appSecret:@"f29df781abdd4f49beca5a2194676ca4"];
-    
-    
-   */
-
-    //监听用户信息变更
-    [ShareSDK addNotificationWithName:SSN_USER_INFO_UPDATE
-                               target:self
-                               action:@selector(userInfoUpdateHandler:)];
-    
-    
-    
     //MARK:JPush
     [APService registerForRemoteNotificationTypes:(UIRemoteNotificationTypeBadge |
                                                    UIRemoteNotificationTypeSound |
                                                    UIRemoteNotificationTypeAlert)];
     [APService setupWithOption:launchOptions];
-    //设置用户名称和房间名
-
     NSNotificationCenter *defaultCenter = [NSNotificationCenter defaultCenter];
     [defaultCenter addObserver:self selector:@selector(networkDidSetup:) name:kAPNetworkDidSetupNotification object:nil];
     [defaultCenter addObserver:self selector:@selector(networkDidClose:) name:kAPNetworkDidCloseNotification object:nil];
@@ -135,6 +99,9 @@
     [defaultCenter addObserver:self selector:@selector(networkDidLogin:) name:kAPNetworkDidLoginNotification object:nil];
     [defaultCenter addObserver:self selector:@selector(networkDidReceiveMessage:) name:kAPNetworkDidReceiveMessageNotification object:nil];
 
+    
+    
+    
     self.window.rootViewController=menuVC;
     [self.window makeKeyAndVisible];
     return YES;
@@ -163,94 +130,6 @@
 - (void)application:(UIApplication *)application willChangeStatusBarOrientation:(UIInterfaceOrientation)newStatusBarOrientation duration:(NSTimeInterval)duration
 {
 }
-//- (void)userInfoUpdateHandler:(NSNotification *)notif
-//{
-//    NSMutableArray *authList = [NSMutableArray arrayWithContentsOfFile:[NSString stringWithFormat:@"%@/authListCache.plist",NSTemporaryDirectory()]];
-//    if (authList == nil)
-//    {
-//        authList = [NSMutableArray array];
-//    }
-//    
-//    NSString *platName = nil;
-//    NSInteger plat = [[[notif userInfo] objectForKey:SSK_PLAT] integerValue];
-//    switch (plat)
-//    {
-//        case ShareTypeSinaWeibo:
-//            platName = NSLocalizedString(@"TEXT_SINA_WEIBO", @"新浪微博");
-//            break;
-//        case ShareType163Weibo:
-//            platName = NSLocalizedString(@"TEXT_NETEASE_WEIBO", @"网易微博");
-//            break;
-//        case ShareTypeDouBan:
-//            platName = NSLocalizedString(@"TEXT_DOUBAN", @"豆瓣");
-//            break;
-//        case ShareTypeFacebook:
-//            platName = @"Facebook";
-//            break;
-//        case ShareTypeKaixin:
-//            platName = NSLocalizedString(@"TEXT_KAIXIN", @"开心网");
-//            break;
-//        case ShareTypeQQSpace:
-//            platName = NSLocalizedString(@"TEXT_QZONE", @"QQ空间");
-//            break;
-//        case ShareTypeRenren:
-//            platName = NSLocalizedString(@"TEXT_RENREN", @"人人网");
-//            break;
-//        case ShareTypeSohuWeibo:
-//            platName = NSLocalizedString(@"TEXT_SOHO_WEIBO", @"搜狐微博");
-//            break;
-//        case ShareTypeTencentWeibo:
-//            platName = NSLocalizedString(@"TEXT_TENCENT_WEIBO", @"腾讯微博");
-//            break;
-//        case ShareTypeTwitter:
-//            platName = @"Twitter";
-//            break;
-//        case ShareTypeInstapaper:
-//            platName = @"Instapaper";
-//            break;
-//        case ShareTypeYouDaoNote:
-//            platName = NSLocalizedString(@"TEXT_YOUDAO_NOTE", @"有道云笔记");
-//            break;
-//        case ShareTypeGooglePlus:
-//            platName = @"Google+";
-//            break;
-//        case ShareTypeLinkedIn:
-//            platName = @"LinkedIn";
-//            break;
-//        default:
-//            platName = NSLocalizedString(@"TEXT_UNKNOWN", @"未知");
-//    }
-//    
-//    id<ISSPlatformUser> userInfo = [[notif userInfo] objectForKey:SSK_USER_INFO];
-//    BOOL hasExists = NO;
-//    for (int i = 0; i < [authList count]; i++)
-//    {
-//        NSMutableDictionary *item = [authList objectAtIndex:i];
-//        ShareType type = (ShareType)[[item objectForKey:@"type"] integerValue];
-//        if (type == plat)
-//        {
-//            [item setObject:[userInfo nickname] forKey:@"username"];
-//            hasExists = YES;
-//            break;
-//        }
-//    }
-//    
-//    if (!hasExists)
-//    {
-//        NSDictionary *newItem = [NSMutableDictionary dictionaryWithObjectsAndKeys:
-//                                 platName,
-//                                 @"title",
-//                                 [NSNumber numberWithInteger:plat],
-//                                 @"type",
-//                                 [userInfo nickname],
-//                                 @"username",
-//                                 nil];
-//        [authList addObject:newItem];
-//    }
-//    
-//    [authList writeToFile:[NSString stringWithFormat:@"%@/authListCache.plist",NSTemporaryDirectory()] atomically:YES];
-//}
-
 #pragma mark- applecation
 //友盟分享需加内容
 - (void)applicationDidBecomeActive:(UIApplication *)application
@@ -288,16 +167,12 @@
 
 #pragma mark- AliPay 
 //独立客户端回调函数
-
-
 - (void)parse:(NSURL *)url application:(UIApplication *)application {
     
     //结果处理
     AlixPayResult* result = [self handleOpenURL:url];
-    
 	if (result)
     {
-		
 		if (result.statusCode == 9000)
         {
 			/*
@@ -370,24 +245,23 @@
 
 #pragma mark - 远程推送接收
 - (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo {
-    
     // Required
     [APService handleRemoteNotification:userInfo];
 }
-
 - (void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken {
-    
     // Required
     [APService registerDeviceToken:deviceToken];
 }
+- (void)application:(UIApplication *)application didFailToRegisterForRemoteNotificationsWithError:(NSError *)error
+{
 
+}
 #pragma mark- 本地推送接收
 -(void)application:(UIApplication *)application didReceiveLocalNotification:(UILocalNotification *)notification
 {
     NSDictionary *dict = notification.userInfo;
     NSLog(@"dict:%@",dict);
 }
-
 #pragma mark - 推送消息log
 
 - (void)networkDidSetup:(NSNotification *)notification {
@@ -442,7 +316,6 @@
     [row setObject:newHtmlStr forKey:@"text"];
     [[NSNotificationCenter defaultCenter] postNotificationName:@"messagereceived" object:row];
 }
-
 - (void)tagsAliasCallback:(int)iResCode tags:(NSSet*)tags alias:(NSString*)alias {
     NSLog(@"rescode: %d, \ntags: %@, \nalias: %@\n", iResCode, tags , alias);
 }
