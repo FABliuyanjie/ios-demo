13dd609482ab9249eb33ee39addf7468847adea5
李正峰 <tmdqq01@gmail.com>
2014-05-23 18:13:27 +0800

    删除ShareSDK，大幅修改TOOL中验证的实现

---
+++ ScreenShow/我/mysub/帐号管理/第三方授权管理/AccountBindViewController.h
@@ -0,0 +1,13 @@
+//
+//  AccountBindViewController.h
+//  ScreenShow
+//
+//  Created by 李正峰 on 14-5-22.
+//  Copyright (c) 2014年 lee. All rights reserved.
+//
+
+#import "BaseTableViewController.h"
+
+@interface AccountBindViewController : BaseTableViewController
+
+@end
