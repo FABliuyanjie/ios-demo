0a9f967a16c2f802eaba7bd9f23e586770b4b04a
FABliuyanjie <yj8265126>
2014-05-26 17:25:52 +0800

    登录之后第三方绑定界面
    登录之后第三方绑定界面（绑定和接触绑定还没完成）

---
+++ ScreenShow/ScreenShow/models/AuthType.m
@@ -0,0 +1,13 @@
+//
+//  AuthType.m
+//  ScreenShow
+//
+//  Created by 刘艳杰 on 14-5-26.
+//  Copyright (c) 2014年 lee. All rights reserved.
+//
+
+#import "AuthType.h"
+
+@implementation AuthType
+
+@end
